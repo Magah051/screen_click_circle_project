1240ab6f6dc0d73fc5ba7f7ff16756541240d128
José Magalhães <joses.magalhaes@hotmail.com>
2023-12-19 12:11:34 -0300

    :bug: fix: Correção de bug de estilização de circulos

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import './Circle.css';
 
 const Circle = ({ x, y }) => {
-  return <div className="circle" style={{ left: x, top: y }}></div>;
+  return (
+    <div
+      style={{
+        width: '30px',
+        height: '30px',
+        borderRadius: '50%',
+        background: 'blue',
+        position: 'absolute',
+        top: `${y}px`,
+        left: `${x}px`,
+      }}
+    ></div>
+  );
 };
+
 export default Circle;
