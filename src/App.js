54120b46db2a67c34d0454f1f9f0b19aecb4daac
José Magalhães <joses.magalhaes@hotmail.com>
2023-12-19 12:29:23 -0300

    :sparkles: feat: Implementação de botão desfazer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import Circle from './components/Circle';
 
 const App = () => {
   const [circles, setCircles] = useState([]);
+  const [undoStack, setUndoStack] = useState([]);
+  const [redoStack, setRedoStack] = useState([]);
 
   const handleClick = (event) => {
     const { pageX, pageY } = event;
     const newCircle = { x: pageX, y: pageY };
     setCircles([...circles, newCircle]);
+    setUndoStack([...circles, newCircle]);
+    setRedoStack([]);
+  };
+
+  const handleUndo = () => {
+    const lastCircle = undoStack.pop();
+    if (lastCircle) {
+      setRedoStack([...redoStack, lastCircle]);
+      setCircles([...circles.slice(0, -1)]);
+      setUndoStack([...circles.slice(0, -1)]);
+    }
   };
 
   return (
-    <div style={{ position: 'relative', height: '100vh' }} onClick={handleClick}>
-      {circles.map((circle, index) => (
-        <Circle key={index} x={circle.x} y={circle.y} />
-      ))}
+    <div style={{ position: 'relative', height: '100vh' }}>
+      <button onClick={handleUndo} disabled={undoStack.length === 0}>
+        Desfazer
+      </button>
+      <div onClick={handleClick} style={{ height: '100%' }}>
+        {circles.map((circle, index) => (
+          <Circle key={index} x={circle.x} y={circle.y} />
+        ))}
+      </div>
     </div>
   );
 };
